5c26e995b8989179dcf27b1fd296287e7e966557
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-21 03:25:29 -0600

    creating-back-end-final-project

---
+++ src/config/app.js
@@ -0,0 +1,14 @@
+//1. imports and export
+import express from "express"
+
+//2. const for express
+const app = express()
+
+//3. middleware
+app.use(express.json())
+
+//4. routes
+
+
+//1. export
+export default app
