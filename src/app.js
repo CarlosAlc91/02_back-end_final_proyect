dcdd425a04b0a54e703b05c07d41784d4aef2808
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-10-23 03:15:06 -0600

    users-errors-created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 //1. imports and export
 import express from "express"
-//4. routes
+//4. import routes
 import { router } from "./routes/routes.js"
+//6. import errorss
+import { AppError } from "./errors/appError.js"
+import { globalErrorHandler } from "./errors/index.js"
 
 //2. const for express
 const app = express()
@@ -11,6 +14,14 @@ app.use(express.json())
 
 //5. routes e router imoprt
 app.use('/api/v1', router)
+
+//7. errors
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
+
+//8. errores van al controlador de errores
+app.use(globalErrorHandler)
 
 //1. export
 export default app
--- src/errors/index.js
+++ src/errors/index.js
@@ -1,4 +1,9 @@
 //1. export from catchAsync
 export * from './catchAsync.js'
+//3. export appError
+export * from './appError.js'
+//4. export errorcontroller
+export * from './error.controller.js'
 
 //2. go to users.controller
+//3. go to error.controller.js
