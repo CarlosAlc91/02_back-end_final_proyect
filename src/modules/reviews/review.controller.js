35d198004300a9210c72145e4601d96accaa7603
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-11-02 06:01:40 -0600

    reviews-controller-created

--- src/modules/reviews/review.controller.js
+++ src/modules/reviews/review.controller.js
@@ -1,22 +1,88 @@
-//importar catchAsync
+import { AppError, catchAsync } from '../../errors/index.js'
+import { validateReview } from './review.schema.js'
+import { ReviewService } from "./review.service.js"
+
+const reviewService = new ReviewService()
 
 
-export const findAll = catchAsync(async(req, res, next) => {
-    return res.status(200).json(/* valor a retornar */)
-});
+export const createReview = catchAsync(async (req, res, next) => {
+  const {
+    hasError,
+    errorMessage,
+    reviewData
+  } = validateReview(req.body)
 
-export const create = catchAsync(async(req, res, next) => {
-    return res.status(201).json(/* valor a retornar */)
-});
+  if (hasError) {
+    return res.status(422).json({
+      status: 'error',
+      message: errorMessage
+    })
+  }
 
-export const findOne = catchAsync(async(req, res, next) => {
-    return res.status(200).json(/* valor a retornar */)
-});
+  const { comment, rating } = reviewData
+  const { id } = req.params
+  const { sessionUser } = req
 
-export const update = catchAsync(async(req, res, next) => {
-    return res.status(200).json(/* valor a retornar */)
-});
+  const review = await reviewService.createReview({
+    comment,
+    rating,
+    restaurantId: id,
+    userId: sessionUser.id
+  })
 
-export const deletectrl = catchAsync(async(req, res, next) => {
-    return res.status(200).json(/* valor a retornar */)
-});
+  return res.status(201).json(/* valor a retornar */)
+})
+
+
+export const updateReview = catchAsync(async (req, res, next) => {
+  const { review } = req
+  const { restaurant } = req
+  const { sessionUser } = req
+
+  if (sessionUser.id !== review.id) {
+    return next(
+      new AppError('Changes not allowed', 401)
+    )
+  }
+
+  const {
+    hasError,
+    erroMessages,
+    reviewData
+  } = validateReview(req.body)
+
+  if (hasError) {
+    return res.status(422).json({
+      status: 'error',
+      message: erroMessages
+    })
+  }
+
+  if (review.restaurantId !== restaurant.id) {
+    return next(
+      new AppError('Try another restaurant id', 401)
+    )
+  }
+
+  const reviewUpdated = await reviewService.updateReview(review, reviewData)
+  return res.status(200).json(reviewUpdated)
+})
+
+export const deleteReview = catchAsync(async (req, res, next) => {
+  const { review } = req
+  const { restaurant } = req
+  const { sessionUser } = req
+
+  if (sessionUser.id !== review.userId) {
+    return next(new AppError('Changes not allowed', 401))
+  }
+
+  if (review.restaurantId !== restaurant.id) {
+    return next(
+      new AppError('Try another restaurant id', 401)
+    )
+  }
+
+  await reviewService.deleteReview(review)
+  return res.status(200).json(null)
+})
