97fb288fa5f22e56f1293b2e7d3716731a77abaa
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-11-02 05:18:06 -0600

    orders-middleware-created

---
+++ src/modules/orders/orders.middleware.js
@@ -0,0 +1,18 @@
+import { or } from 'sequelize'
+import { AppError, catchAsync } from '../../errors/index.js'
+import { OrdersService } from './orders.service.js'
+
+const ordersService = new OrdersService
+
+export const validateOrder = catchAsync(async (req, res, next) => {
+  const { id } = req.params
+
+  const order = await ordersService.findOrderById(id)
+
+  if (!order) {
+    return next(new AppError(`Order with id ${id} not found`, 401))
+  }
+
+  req.order = order
+  next()
+})
